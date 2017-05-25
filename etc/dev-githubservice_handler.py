98fd1046cbf8a4162b33a52b5fe3ffca59de685f
Ayman Mousa <ayman.mousa@teradata.com>
2017-05-25 16:41:11 -0700

    first commit

--- etc/dev-githubservice_handler.py
+++ etc/dev-githubservice_handler.py
@@ -23,22 +23,22 @@ except ImportError:
     raise ImportError('Missing dependency pyyaml. \
                       Do ``pip install pyyaml``.')
 
-# dev-github configuration
+# dev_github configuration
 
-dev-github_API_BASE_URL = None
-dev-github_AUTH_BASE_URL = None
-dev-github_USERNAME = None
-dev-github_PASSWORD = None
-dev-github_API_KEY = None
-dev-github_AUTH_TOKEN = None
-dev-github_SSL_VERIFY = False
+dev_github_API_BASE_URL = None
+dev_github_AUTH_BASE_URL = None
+dev_github_USERNAME = None
+dev_github_PASSWORD = None
+dev_github_API_KEY = None
+dev_github_AUTH_TOKEN = None
+dev_github_SSL_VERIFY = False
 
-dev-github_AUTH_PATH = 'tokens'
-dev-github_WEBHOOKS_PATH = 'webhooks/dev-github'
-dev-github_TRIGGERS_PATH = 'triggertypes'
-dev-github_TRIGGERTYPE_PACK = 'nagios'
-dev-github_TRIGGERTYPE_NAME = 'service_state_change'
-dev-github_TRIGGERTYPE_REF = '.'.join([dev-github_TRIGGERTYPE_PACK, dev-github_TRIGGERTYPE_NAME])
+dev_github_AUTH_PATH = 'tokens'
+dev_github_WEBHOOKS_PATH = 'webhooks/dev_github'
+dev_github_TRIGGERS_PATH = 'triggertypes'
+dev_github_TRIGGERTYPE_PACK = 'nagios'
+dev_github_TRIGGERTYPE_NAME = 'service_state_change'
+dev_github_TRIGGERTYPE_REF = '.'.join([dev_github_TRIGGERTYPE_PACK, dev_github_TRIGGERTYPE_NAME])
 
 STATE_MESSAGE = {
     'OK': 'All is well on the Western front.',
@@ -47,7 +47,7 @@ STATE_MESSAGE = {
     'CRITICAL': 'Critical!'
 }
 
-REGISTERED_WITH_dev-github = False
+REGISTERED_WITH_dev_github = False
 UNAUTHED = False
 IS_API_KEY_AUTH = False
 
@@ -55,19 +55,19 @@ OK_CODES = [httplib.OK, httplib.CREATED, httplib.ACCEPTED, httplib.CONFLICT]
 UNREACHABLE_CODES = [httplib.NOT_FOUND]
 
 TOKEN_AUTH_HEADER = 'X-Auth-Token'
-API_KEY_AUTH_HEADER = 'dev-github-Api-Key'
+API_KEY_AUTH_HEADER = 'dev_github-Api-Key'
 
 
 def _create_trigger_type(verbose=False):
     try:
-        url = _get_dev-github_triggers_base_url()
+        url = _get_dev_github_triggers_base_url()
         payload = {
-            'name': dev-github_TRIGGERTYPE_NAME,
-            'pack': dev-github_TRIGGERTYPE_PACK,
+            'name': dev_github_TRIGGERTYPE_NAME,
+            'pack': dev_github_TRIGGERTYPE_PACK,
             'description': 'Trigger type for nagios event handler.'
         }
 
-        headers = _get_dev-github_request_headers()
+        headers = _get_dev_github_request_headers()
         headers['Content-Type'] = 'application/json; charset=utf-8'
 
         if verbose:
@@ -76,10 +76,10 @@ def _create_trigger_type(verbose=False):
 
         post_resp = requests.post(url, data=json.dumps(payload),
                                   headers=headers,
-                                  verify=dev-github_SSL_VERIFY)
+                                  verify=dev_github_SSL_VERIFY)
     except:
         traceback.print_exc(limit=20)
-        raise Exception('Unable to connect to dev-github endpoint {0}.'.format(url))
+        raise Exception('Unable to connect to dev_github endpoint {0}.'.format(url))
     else:
         status = post_resp.status_code
         if status in UNREACHABLE_CODES:
@@ -88,16 +88,16 @@ def _create_trigger_type(verbose=False):
             raise Exception(msg)
 
         if status not in OK_CODES:
-            msg = 'Failed to register trigger type {0}.{1} with dev-github. ' \
-                'HTTP_CODE: {2}'.format(dev-github_TRIGGERTYPE_PACK,
-                                        dev-github_TRIGGERTYPE_NAME, status)
+            msg = 'Failed to register trigger type {0}.{1} with dev_github. ' \
+                'HTTP_CODE: {2}'.format(dev_github_TRIGGERTYPE_PACK,
+                                        dev_github_TRIGGERTYPE_NAME, status)
             raise Exception(msg)
         else:
-            print('Registered trigger type with dev-github.\n')
+            print('Registered trigger type with dev_github.\n')
 
 
 def _get_auth_url():
-    return urljoin(dev-github_AUTH_BASE_URL, dev-github_AUTH_PATH)
+    return urljoin(dev_github_AUTH_BASE_URL, dev_github_AUTH_PATH)
 
 
 def _get_auth_token(verbose=False):
@@ -108,11 +108,11 @@ def _get_auth_token(verbose=False):
 
     try:
         resp = requests.post(auth_url, json.dumps({'ttl': 5 * 60}),
-                             auth=(dev-github_USERNAME, dev-github_PASSWORD),
-                             verify=dev-github_SSL_VERIFY)
+                             auth=(dev_github_USERNAME, dev_github_PASSWORD),
+                             verify=dev_github_SSL_VERIFY)
     except:
         traceback.print_exc(limit=20)
-        raise Exception('Unable to connect to dev-github endpoint {0}.'.
+        raise Exception('Unable to connect to dev_github endpoint {0}.'.
                         format(auth_url))
     else:
         if resp.status_code in UNREACHABLE_CODES:
@@ -128,49 +128,49 @@ def _get_auth_token(verbose=False):
         return resp.json()['token']
 
 
-def _get_dev-github_request_headers():
+def _get_dev_github_request_headers():
     headers = {}
 
     if not UNAUTHED:
         if IS_API_KEY_AUTH:
-            headers[API_KEY_AUTH_HEADER] = dev-github_API_KEY
+            headers[API_KEY_AUTH_HEADER] = dev_github_API_KEY
         else:
-            if dev-github_AUTH_TOKEN:
-                headers[TOKEN_AUTH_HEADER] = dev-github_AUTH_TOKEN
+            if dev_github_AUTH_TOKEN:
+                headers[TOKEN_AUTH_HEADER] = dev_github_AUTH_TOKEN
             else:
                 pass
 
     return headers
 
 
-def _register_with_dev-github(verbose=False):
-    global REGISTERED_WITH_dev-github
+def _register_with_dev_github(verbose=False):
+    global REGISTERED_WITH_dev_github
     try:
-        if not REGISTERED_WITH_dev-github:
+        if not REGISTERED_WITH_dev_github:
             if verbose:
-                print('Checking if trigger "{0}" registered with dev-github.'
-                      .format(dev-github_TRIGGERTYPE_REF))
-            _register_trigger_with_dev-github(verbose=verbose)
-            REGISTERED_WITH_dev-github = True
+                print('Checking if trigger "{0}" registered with dev_github.'
+                      .format(dev_github_TRIGGERTYPE_REF))
+            _register_trigger_with_dev_github(verbose=verbose)
+            REGISTERED_WITH_dev_github = True
     except:
         traceback.print_exc(limit=20)
         sys.stderr.write(
-            'Failed registering with dev-github. Won\'t post event.\n')
+            'Failed registering with dev_github. Won\'t post event.\n')
         sys.exit(2)
 
 
-def _register_trigger_with_dev-github(verbose=False):
-    triggers_url = _get_dev-github_triggers_url()
+def _register_trigger_with_dev_github(verbose=False):
+    triggers_url = _get_dev_github_triggers_url()
 
     try:
-        headers = _get_dev-github_request_headers()
+        headers = _get_dev_github_request_headers()
         if verbose:
             print('Will GET from URL {0} for detecting trigger {1}.\n'
-                  .format(triggers_url, dev-github_TRIGGERTYPE_REF))
+                  .format(triggers_url, dev_github_TRIGGERTYPE_REF))
             print('Request headers: {0}\n'.format(headers))
 
         get_resp = requests.get(triggers_url, headers=headers,
-                                verify=dev-github_SSL_VERIFY)
+                                verify=dev_github_SSL_VERIFY)
 
         if get_resp.status_code != httplib.OK:
             _create_trigger_type(verbose=verbose)
@@ -180,32 +180,32 @@ def _register_trigger_with_dev-github(verbose=False):
                 _create_trigger_type(verbose=verbose)
     except:
         traceback.print_exc(limit=20)
-        raise Exception('Unable to connect to dev-github endpoint {0}.\n'
+        raise Exception('Unable to connect to dev_github endpoint {0}.\n'
                         .format(triggers_url))
     else:
         if verbose:
-            print('Successfully registered trigger {0} with dev-github.\n'
-                  .format(dev-github_TRIGGERTYPE_REF))
+            print('Successfully registered trigger {0} with dev_github.\n'
+                  .format(dev_github_TRIGGERTYPE_REF))
 
 
-def _get_dev-github_triggers_base_url():
-    url = urljoin(dev-github_API_BASE_URL, dev-github_TRIGGERS_PATH)
+def _get_dev_github_triggers_base_url():
+    url = urljoin(dev_github_API_BASE_URL, dev_github_TRIGGERS_PATH)
     return url
 
 
-def _get_dev-github_triggers_url():
-    url = urljoin(_get_dev-github_triggers_base_url() + '/', dev-github_TRIGGERTYPE_REF)
+def _get_dev_github_triggers_url():
+    url = urljoin(_get_dev_github_triggers_base_url() + '/', dev_github_TRIGGERTYPE_REF)
     return url
 
 
-def _get_dev-github_webhooks_url():
-    url = urljoin(dev-github_API_BASE_URL, dev-github_WEBHOOKS_PATH)
+def _get_dev_github_webhooks_url():
+    url = urljoin(dev_github_API_BASE_URL, dev_github_WEBHOOKS_PATH)
     return url
 
 
 def _post_webhook(url, body, verbose=False):
-    headers = _get_dev-github_request_headers()
-    headers['X-dev-github-Integration'] = 'nagios.'
+    headers = _get_dev_github_request_headers()
+    headers['X-dev_github-Integration'] = 'nagios.'
     headers['Content-Type'] = 'application/json; charset=utf-8'
 
     try:
@@ -216,7 +216,7 @@ def _post_webhook(url, body, verbose=False):
                           verify=False)
     except:
         traceback.print_exc(10)
-        raise Exception('Cannot connect to dev-github endpoint {0}.'.format(url))
+        raise Exception('Cannot connect to dev_github endpoint {0}.'.format(url))
     else:
         status = r.status_code
 
@@ -226,35 +226,35 @@ def _post_webhook(url, body, verbose=False):
             raise Exception(msg)
 
         if status not in OK_CODES:
-            sys.stderr.write('Failed posting nagio event to dev-github. HTTP_CODE: '
+            sys.stderr.write('Failed posting nagio event to dev_github. HTTP_CODE: '
                              '{0}\n'.format(status))
         else:
-            sys.stdout.write('Sent nagios event to dev-github. HTTP_CODE: '
+            sys.stdout.write('Sent nagios event to dev_github. HTTP_CODE: '
                              '{0}\n'.format(status))
 
 
-def _post_event_to_dev-github(payload, verbose=False):
+def _post_event_to_dev_github(payload, verbose=False):
     body = {}
-    body['trigger'] = dev-github_TRIGGERTYPE_REF
+    body['trigger'] = dev_github_TRIGGERTYPE_REF
     body['payload'] = payload
 
     try:
-        _post_webhook(url=_get_dev-github_webhooks_url(), body=body, verbose=verbose)
+        _post_webhook(url=_get_dev_github_webhooks_url(), body=body, verbose=verbose)
         return True
     except:
         traceback.print_exc(limit=10)
-        print('Cannot send event to dev-github.')
+        print('Cannot send event to dev_github.')
         sys.exit(3)
 
 
 def _set_config_opts(config_file, verbose=False):
-    global dev-github_USERNAME
-    global dev-github_PASSWORD
-    global dev-github_API_KEY
-    global dev-github_AUTH_TOKEN
-    global dev-github_API_BASE_URL
-    global dev-github_AUTH_BASE_URL
-    global dev-github_SSL_VERIFY
+    global dev_github_USERNAME
+    global dev_github_PASSWORD
+    global dev_github_API_KEY
+    global dev_github_AUTH_TOKEN
+    global dev_github_API_BASE_URL
+    global dev_github_AUTH_BASE_URL
+    global dev_github_SSL_VERIFY
     global UNAUTHED
     global IS_API_KEY_AUTH
     if not os.path.exists(config_file):
@@ -268,32 +268,32 @@ def _set_config_opts(config_file, verbose=False):
         if verbose:
             print('Contents of config file: {0}'.format(config))
 
-        dev-github_USERNAME = config['dev-github_username']
-        dev-github_PASSWORD = config['dev-github_password']
-        dev-github_API_KEY = config.get('dev-github_api_key', None)
-        dev-github_API_BASE_URL = config['dev-github_api_base_url']
-        if not dev-github_API_BASE_URL.endswith('/'):
-            dev-github_API_BASE_URL += '/'
-        dev-github_AUTH_BASE_URL = config['dev-github_auth_base_url']
-        if not dev-github_AUTH_BASE_URL.endswith('/'):
-            dev-github_AUTH_BASE_URL += '/'
+        dev_github_USERNAME = config['dev_github_username']
+        dev_github_PASSWORD = config['dev_github_password']
+        dev_github_API_KEY = config.get('dev_github_api_key', None)
+        dev_github_API_BASE_URL = config['dev_github_api_base_url']
+        if not dev_github_API_BASE_URL.endswith('/'):
+            dev_github_API_BASE_URL += '/'
+        dev_github_AUTH_BASE_URL = config['dev_github_auth_base_url']
+        if not dev_github_AUTH_BASE_URL.endswith('/'):
+            dev_github_AUTH_BASE_URL += '/'
         UNAUTHED = config['unauthed']
-        dev-github_SSL_VERIFY = config['ssl_verify']
+        dev_github_SSL_VERIFY = config['ssl_verify']
 
-    if dev-github_API_KEY:
+    if dev_github_API_KEY:
         IS_API_KEY_AUTH = True
 
     if verbose:
         print('Unauthed? : {0}\nAPI key auth?: {1}\nSSL Verify? : {2}\n'
-              .format(UNAUTHED, IS_API_KEY_AUTH, dev-github_SSL_VERIFY))
+              .format(UNAUTHED, IS_API_KEY_AUTH, dev_github_SSL_VERIFY))
 
     if not UNAUTHED and not IS_API_KEY_AUTH:
         try:
-            if not dev-github_AUTH_TOKEN:
+            if not dev_github_AUTH_TOKEN:
                 if verbose:
                     print('No auth token found. Let\'s get one from'
                           'StackStorm!')
-                dev-github_AUTH_TOKEN = _get_auth_token(verbose=verbose)
+                dev_github_AUTH_TOKEN = _get_auth_token(verbose=verbose)
         except:
             traceback.print_exc(limit=20)
             print('Unable to negotiate an auth token. Exiting!')
@@ -329,8 +329,8 @@ def _from_arg_to_payload(nagios_args):
 def main(config_file, payload, verbose=False):
 
     _set_config_opts(config_file=config_file, verbose=verbose)
-    _register_with_dev-github(verbose=verbose)
-    _post_event_to_dev-github(payload, verbose=verbose)
+    _register_with_dev_github(verbose=verbose)
+    _post_event_to_dev_github(payload, verbose=verbose)
 
 
 if __name__ == '__main__':
--- tests/test_nagios_handler.py
+++ tests/test_nagios_handler.py
@@ -3,9 +3,9 @@ import json
 import mock
 import requests
 import responses
-import unittedev-github
+import unittedev_github
 
-import dev-githubservice_handler as nagios_handler
+import dev_githubservice_handler as nagios_handler
 
 
 __all__ = [
@@ -27,49 +27,49 @@ class FakeResponse(object):
         raise Exception(self.reason)
 
 
-class NagiosHandlerTestCase(unittedev-github.TestCase):
+class NagiosHandlerTestCase(unittedev_github.TestCase):
 
-    def test_dev-github_headers_token_auth(self):
+    def test_dev_github_headers_token_auth(self):
         nagios_handler.IS_API_KEY_AUTH = False
         self.assertEqual(nagios_handler.IS_API_KEY_AUTH, False,
                          'API auth should be off for this test.')
-        nagios_handler.dev-github_AUTH_TOKEN = 'dummy-token'
-        nagios_handler.dev-github_API_KEY = 'dummy-api-key'
-        headers = nagios_handler._get_dev-github_request_headers()
+        nagios_handler.dev_github_AUTH_TOKEN = 'dummy-token'
+        nagios_handler.dev_github_API_KEY = 'dummy-api-key'
+        headers = nagios_handler._get_dev_github_request_headers()
         self.assertTrue('X-Auth-Token' in headers)
-        self.assertTrue('dev-github-Api-Key' not in headers)
+        self.assertTrue('dev_github-Api-Key' not in headers)
         self.assertEqual(headers['X-Auth-Token'], 'dummy-token')
 
-    def test_dev-github_headers_apikey_auth(self):
+    def test_dev_github_headers_apikey_auth(self):
         nagios_handler.IS_API_KEY_AUTH = True
         self.assertEqual(nagios_handler.IS_API_KEY_AUTH, True,
                          'API auth should be on for this test.')
-        nagios_handler.dev-github_AUTH_TOKEN = 'dummy-token'
-        nagios_handler.dev-github_API_KEY = 'dummy-api-key'
-        headers = nagios_handler._get_dev-github_request_headers()
+        nagios_handler.dev_github_AUTH_TOKEN = 'dummy-token'
+        nagios_handler.dev_github_API_KEY = 'dummy-api-key'
+        headers = nagios_handler._get_dev_github_request_headers()
         self.assertTrue('X-Auth-Token' not in headers)
-        self.assertTrue('dev-github-Api-Key' in headers)
-        self.assertEqual(headers['dev-github-Api-Key'], 'dummy-api-key')
+        self.assertTrue('dev_github-Api-Key' in headers)
+        self.assertEqual(headers['dev_github-Api-Key'], 'dummy-api-key')
 
-    def test_get_dev-github_auth_url(self):
-        nagios_handler.dev-github_AUTH_BASE_URL = 'https://localhost/auth/v1/'
+    def test_get_dev_github_auth_url(self):
+        nagios_handler.dev_github_AUTH_BASE_URL = 'https://localhost/auth/v1/'
         self.assertEqual(nagios_handler._get_auth_url(),
                          'https://localhost/auth/v1/tokens')
 
-    def test_get_dev-github_triggers_base_url(self):
-        nagios_handler.dev-github_API_BASE_URL = 'https://localhost/api/v1/'
-        self.assertEqual(nagios_handler._get_dev-github_triggers_base_url(),
+    def test_get_dev_github_triggers_base_url(self):
+        nagios_handler.dev_github_API_BASE_URL = 'https://localhost/api/v1/'
+        self.assertEqual(nagios_handler._get_dev_github_triggers_base_url(),
                          'https://localhost/api/v1/triggertypes')
 
-    def test_get_dev-github_triggers_url(self):
-        nagios_handler.dev-github_API_BASE_URL = 'https://localhost/api/v1/'
-        self.assertEqual(nagios_handler._get_dev-github_triggers_url(),
+    def test_get_dev_github_triggers_url(self):
+        nagios_handler.dev_github_API_BASE_URL = 'https://localhost/api/v1/'
+        self.assertEqual(nagios_handler._get_dev_github_triggers_url(),
                          'https://localhost/api/v1/triggertypes/nagios.service_state_change')
 
-    def test_get_dev-github_webhooks_url(self):
-        nagios_handler.dev-github_API_BASE_URL = 'https://localhost/api/v1/'
-        self.assertEqual(nagios_handler._get_dev-github_webhooks_url(),
-                         'https://localhost/api/v1/webhooks/dev-github')
+    def test_get_dev_github_webhooks_url(self):
+        nagios_handler.dev_github_API_BASE_URL = 'https://localhost/api/v1/'
+        self.assertEqual(nagios_handler._get_dev_github_webhooks_url(),
+                         'https://localhost/api/v1/webhooks/dev_github')
 
     def test_from_arg_to_payload(self):
         nagios_args = ['foo', 'bar']
@@ -79,7 +79,7 @@ class NagiosHandlerTestCase(unittedev-github.TestCase):
 
     @responses.activate
     def test_get_auth_token(self):
-        nagios_handler.dev-github_AUTH_BASE_URL = 'https://localhost/auth/v1/'
+        nagios_handler.dev_github_AUTH_BASE_URL = 'https://localhost/auth/v1/'
         responses.add(
             responses.POST, 'https://localhost/auth/v1/tokens',
             json={'token': 'your_auth_token'}, status=202
@@ -87,32 +87,32 @@ class NagiosHandlerTestCase(unittedev-github.TestCase):
         token = nagios_handler._get_auth_token()
         self.assertEqual(token, 'your_auth_token')
 
-    @mock.patch('dev-githubservice_handler._create_trigger_type')
+    @mock.patch('dev_githubservice_handler._create_trigger_type')
     @responses.activate
     def test_get_trigger_type_trigger_exists(self, mock_method):
-        nagios_handler.dev-github_API_BASE_URL = 'https://localhost/api/v1/'
+        nagios_handler.dev_github_API_BASE_URL = 'https://localhost/api/v1/'
         responses.add(
             responses.GET, 'https://localhost/api/v1/triggertypes/nagios.service_state_change',
             json={'type': 'nagios.service_state_change'}, status=200
         )
-        nagios_handler._register_trigger_with_dev-github()
+        nagios_handler._register_trigger_with_dev_github()
         self.assertFalse(mock_method.called)
 
-    @mock.patch('dev-githubservice_handler._create_trigger_type')
+    @mock.patch('dev_githubservice_handler._create_trigger_type')
     @responses.activate
     def test_trigger_creation_trigger_not_exists(self, mock_method):
-        nagios_handler.dev-github_API_BASE_URL = 'https://localhost/api/v1/'
+        nagios_handler.dev_github_API_BASE_URL = 'https://localhost/api/v1/'
         responses.add(
             responses.GET, 'https://localhost/api/v1/triggertypes/nagios.service_state_change',
             json={}, status=404
         )
-        nagios_handler._register_trigger_with_dev-github()
+        nagios_handler._register_trigger_with_dev_github()
         self.assertTrue(mock_method.called)
 
     @mock.patch.object(requests, 'post', mock.MagicMock(
         return_value=FakeResponse(json.dumps({}), status_code=200, reason='blah')))
     def test_create_trigger_type(self):
-        nagios_handler.dev-github_API_BASE_URL = 'https://localhost/api/v1/'
+        nagios_handler.dev_github_API_BASE_URL = 'https://localhost/api/v1/'
         nagios_handler._create_trigger_type()
         requests.post.assert_called_once_with('https://localhost/api/v1/triggertypes',
             data='{"description": "Trigger type for nagios event handler.", ' +
@@ -122,8 +122,8 @@ class NagiosHandlerTestCase(unittedev-github.TestCase):
     @mock.patch.object(requests, 'post', mock.MagicMock(
         return_value=FakeResponse(json.dumps({}), status_code=200, reason='blah')))
     def test_ssl_verify_on(self):
-        nagios_handler.dev-github_API_BASE_URL = 'https://localhost/api/v1/'
-        nagios_handler.dev-github_SSL_VERIFY = True
+        nagios_handler.dev_github_API_BASE_URL = 'https://localhost/api/v1/'
+        nagios_handler.dev_github_SSL_VERIFY = True
         nagios_handler._create_trigger_type()
         requests.post.assert_called_with('https://localhost/api/v1/triggertypes',
             data='{"description": "Trigger type for nagios event handler.", ' +
@@ -131,9 +131,9 @@ class NagiosHandlerTestCase(unittedev-github.TestCase):
             headers={'Content-Type': 'application/json; charset=utf-8'}, verify=True
         )
 
-    def test_post_event_to_dev-github_bad_api_url(self):
-        nagios_handler.dev-github_API_BASE_URL = 'https://localhost/api'
+    def test_post_event_to_dev_github_bad_api_url(self):
+        nagios_handler.dev_github_API_BASE_URL = 'https://localhost/api'
         trigger_payload = {'foo': 'bar'}
         with self.assertRaises(SystemExit) as cm:
-            nagios_handler._post_event_to_dev-github(json.dumps(trigger_payload))
+            nagios_handler._post_event_to_dev_github(json.dumps(trigger_payload))
         self.assertTrue(cm.exception.code > 0)
